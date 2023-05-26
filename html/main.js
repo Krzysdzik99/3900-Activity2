0372c0fff4a2c0ad41d51c63c0cc2fd9eb8a0b8e
Krzysdzik99 <134332601+Krzysdzik99@users.noreply.github.com>
2023-05-26 02:28:38 -0500

    Moved the Javascript Around
    Moved the javascript around to get the photos to default to ope and updated the CSS to make the header reflective of the need changes.

--- html/main.js
+++ html/main.js
@@ -4,10 +4,10 @@
 window.onscroll = function() {myScrollFunction()};
 
 //Get the top nav bar
-let navbar = document.getElementById("top_nav");
+const navbar = document.getElementById("navbar");
 
 //Get the offset position of the top navbar
-let sticky = navbar.offsetTop;
+const sticky = navbar.offsetTop;
 
 //Add the sticky class to the navbar when you reach its scroll position. It needs to remove the sticky class when the scroll no longer occurs
 function myScrollFunction() {
