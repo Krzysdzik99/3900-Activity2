6cef7405f7761445cc122fd563e07b217f8d0166
Krzysdzik99 <134332601+Krzysdzik99@users.noreply.github.com>
2023-05-26 12:46:38 -0500

    Final Attempt of Fixing Nav Bar Stickiness
    Not a needed product since it is not required by the assignment. I just wanted to try to get some javascript experience. The HTML and CSS is fully functional though. I may return one day to figure why the nav bar is not remaining sticky

--- html/main.js
+++ html/main.js
@@ -1,19 +1,15 @@
-//I attempted my hand at some Javascript but this is my first time using it. I learn this from W3schools
-
-//When the user scrolls on the page, we need the function to execute
-window.onscroll = function() {myScrollFunction()};
-
-//Get the top nav bar
+//I attempted the ability to have the nav bar be sticky but I am having some issues
+//Following the W3 Schools tutorial
+Window.onScroll = function () {
+    myFunction()
+};
 let navbar = document.getElementById("top_nav");
-
-//Get the offset position of the top navbar
 let sticky = navbar.offsetTop;
 
-//Add the sticky class to the navbar when you reach its scroll position. It needs to remove the sticky class when the scroll no longer occurs
-function myScrollFunction() {
+function myFunction () {
     if (window.pageYOffset >= sticky) {
         navbar.classList.add("sticky")
     } else {
-      navbar.classList.remove("sticky");
+        navbar.classList.remove("sticky")
     }
 }
