1c4f655c8406e95dce34833b3b9634f70ec5e70f
Krzysdzik99 <134332601+Krzysdzik99@users.noreply.github.com>
2023-05-26 09:59:00 -0500

    Home Landing Page Completed
    The Home Landing Page is Complete. Headers, Paragraphs, and footers were add to the page and CSS was applied.

--- html/main.js
+++ html/main.js
@@ -4,10 +4,10 @@
 window.onscroll = function() {myScrollFunction()};
 
 //Get the top nav bar
-const navbar = document.getElementById("navbar");
+let navbar = document.getElementById("top_nav");
 
 //Get the offset position of the top navbar
-const sticky = navbar.offsetTop;
+let sticky = navbar.offsetTop;
 
 //Add the sticky class to the navbar when you reach its scroll position. It needs to remove the sticky class when the scroll no longer occurs
 function myScrollFunction() {
